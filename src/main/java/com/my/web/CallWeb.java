c4bca7d377c8d36f39585c00b7e52fec9f7e4345
Administrator <Administrator@172.28.86.2>
2018-07-04 19:44:55 +0800

    Add submit form demo！

--- src/main/java/com/my/web/CallWeb.java
+++ src/main/java/com/my/web/CallWeb.java
@@ -1,6 +1,8 @@
 package com.my.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,21 @@ public class CallWeb extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("班级："+request.getParameter("stuClass")+"<br/>");
+		pw.println("用户名："+request.getParameter("stuName")+"<br/>");
+		pw.println("密码："+request.getParameter("stuPassword")+"<br/>");
+		pw.println("性别："+request.getParameter("sex")+"<br/>");
+		String[] arr=request.getParameterValues("love");
+		for (String string : arr) {
+			pw.println("兴趣："+string+"<br/>");
+		}
+		pw.println("地址："+request.getParameter("address")+"<br/>");
+		
+	
 	}
 
 	/**
