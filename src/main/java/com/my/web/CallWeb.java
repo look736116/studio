f9257e42c598e4b7c0ebc09fb56b324cd1013b4e
Administrator <Administrator@WIN-5LOT13GVU32>
2018-07-07 16:57:25 +0800

    Learn some new knowledge.

--- src/main/java/com/my/web/CallWeb.java
+++ src/main/java/com/my/web/CallWeb.java
@@ -2,12 +2,15 @@ package com.my.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Show
@@ -28,7 +31,12 @@ public class CallWeb extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stuts		
+		//set cookies
+		Cookie cookie = new Cookie("user", "4397881111s");
+		cookie.setMaxAge(60*60*24);
+		response.addCookie(cookie);
+		
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
@@ -37,11 +45,18 @@ public class CallWeb extends HttpServlet {
 		pw.println("用户名："+request.getParameter("stuName")+"<br/>");
 		pw.println("密码："+request.getParameter("stuPassword")+"<br/>");
 		pw.println("性别："+request.getParameter("sex")+"<br/>");
-		String[] arr=request.getParameterValues("love");
+		String[] arr = request.getParameterValues("love");
 		for (String string : arr) {
 			pw.println("兴趣："+string+"<br/>");
 		}
-		pw.println("地址："+request.getParameter("address")+"<br/>");
+		pw.println("地址："+request.getParameter("address")+"<br/>");	
+		
+		HttpSession session = request.getSession();
+		System.out.println(session.isNew());
+		System.out.println(new Date(session.getCreationTime()));
+		session.setAttribute("userName", "abc123");
+		session.setAttribute("password", "456");
+		
 		
 	
 	}
